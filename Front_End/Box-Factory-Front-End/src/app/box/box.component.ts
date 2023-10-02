3bb9388ed1fa50befa93421ce85af4f18ce7cab6
tina <nhi_tran@live.dk>
2023-10-02 14:29:14 +0200

    frontend start

---
+++ Front_End/Box-Factory-Front-End/src/app/box/box.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.scss'],
+})
+export class BoxComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  onEditClick() {
+
+  }
+
+  onDeleteClick() {
+
+  }
+}
--- Front_End/Box-Factory-Front-End/src/app/home/home.module.ts
+++ Front_End/Box-Factory-Front-End/src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {BoxComponent} from "../box/box.component";
+
 
 
 @NgModule({
@@ -14,6 +16,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, BoxComponent]
 })
 export class HomePageModule {}
