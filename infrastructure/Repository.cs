c243f7b26e40bac39b2ea072d338bfc0a64881a0
JulianHesberg <julianhesbergdk@gmail.com>
2023-10-02 10:38:30 +0200

    sql statements

--- infrastructure/Repository.cs
+++ infrastructure/Repository.cs
@@ -6,37 +6,93 @@ public class Repository
 {
     public Box AddBox(Box box)
     {
-        //TO DO
-        return null;
+        var sql = $@"INSERT INTO boxes(width, length, height, volume, material, inventory_count, price)
+                        VALUES(@width, @length, @height, @volume, @material, @inventorycount, @price)
+                        RETURNING
+                        id as {nameof(Box.Id)},
+                        width as {nameof(Box.Width)},
+                        length as {nameof(Box.Length)},
+                        height as {nameof(Box.Height)},
+                        volume as {nameof(Box.Volume)},
+                        material as {nameof(Box.Material)},
+                        inventory_count as {nameof(Box.InventoryCount)},
+                        price as {nameof(Box.Price)};";
+
+        using (var conn = DataConnection.DataSource.OpenConnection())
+        {
+            return conn.QueryFirst<Box>(sql, new
+            {
+                width = box.Width, length = box.Length, height = box.Height, volume = box.Volume,
+                material = box.Material, inventorycount = box.InventoryCount, price = box.Price
+            });
+        }
     }
 
     public Box GetBoxById(int boxId)
     {
-        //TO DO
-        return null;
+        var sql = $@"SELECT * FROM boxes WHERE id = @id;";
+
+        using (var conn = DataConnection.DataSource.OpenConnection())
+        {
+            return conn.QueryFirst<Box>(sql, new { id=boxId });
+        }
     }
 
     public bool DeleteBoxById(int boxId)
     {
-        //TO DO
-        return false;
+        var sql = $@"DELETE FROM boxes WHERE id = @id;";
+
+        using (var conn = DataConnection.DataSource.OpenConnection())
+        {
+            return conn.Execute(sql, new { boxId }) == 1;
+        }
     }
 
     public IEnumerable<Box> GetAllBoxes()
     {
-        //TO DO
-        return null;
+        var sql = $@"SELECT * FROM boxes;";
+
+        using (var conn = DataConnection.DataSource.OpenConnection())
+        {
+            return conn.Query<Box>(sql);
+        }
     }
 
     public Box UpdateBox(Box box)
     {
-        //TO DO
-        return null;
+        var sql = $@"UPDATE boxes
+                        SET width=@width, length=@length, height=@height, volume=@volume,
+                            material=@material, inventory_count=@inventorycount, price=@price 
+                        WHERE id=@id
+                        RETURNING
+                        id as {nameof(Box.Id)},
+                        width as {nameof(Box.Width)},
+                        length as {nameof(Box.Length)},
+                        height as {nameof(Box.Height)},
+                        volume as {nameof(Box.Volume)},
+                        material as {nameof(Box.Material)},
+                        inventory_count as {nameof(Box.InventoryCount)},
+                        price as {nameof(Box.Price)};";
+
+        using (var conn = DataConnection.DataSource.OpenConnection())
+        {
+            return conn.QueryFirst<Box>(sql, new
+            {
+                width = box.Width, length = box.Length, height = box.Height, volume = box.Volume,
+                material = box.Material, inventorycount = box.InventoryCount, price = box.Price, id = box.Id
+            });
+        }
     }
 
     public IEnumerable<Box> SearchForBox(string query)
     {
-        //TO DO
-        return null;
+        var sql = $@"SELECT * FROM boxes 
+                        WHERE LOWER(material) LIKE '%' || @query || '%'
+                        ORDER BY id;";
+
+        using (var conn = DataConnection.DataSource.OpenConnection())
+        {
+            return conn.Query<Box>(sql, new { query });
+        }
     }
 }
