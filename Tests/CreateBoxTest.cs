60e578b8f96294e49703115bf0b92f153d486c75
Madsp15 <madsp@hotmail.dk>
2023-10-04 10:32:33 +0200

    moar tests
    added Delete and Edit box test

--- Tests/CreateBoxTest.cs
+++ Tests/CreateBoxTest.cs
@@ -1,5 +1,5 @@
 ﻿using infrastructure;
-using NUnit.Framework;
+
 
 
 namespace Tests;
@@ -27,22 +27,32 @@ public class CreateBoxTest
             InventoryCount = 50,
             Price = 25.99
         };
-        Box addedBox = repository.AddBox(boxToAdd);
         
-        Assert.NotNull(addedBox);
 
-        var url = "http://localhost:5054/api/boxes";
-
-        Box retrievedBox = repository.GetBoxById(boxToAdd.Id);
-        Assert.NotNull(retrievedBox);
-        Assert.That(retrievedBox.Width, Is.EqualTo(boxToAdd.Width));
-        Assert.That(retrievedBox.Height, Is.EqualTo(boxToAdd.Height));
-        Assert.That(retrievedBox.Length, Is.EqualTo(boxToAdd.Length));
-        Assert.That(retrievedBox.Material,Is.EqualTo(boxToAdd.Material));
-        Assert.That(retrievedBox.Price,Is.EqualTo(boxToAdd.Price));
-        Assert.That(retrievedBox.InventoryCount,Is.EqualTo(boxToAdd.InventoryCount));
-        Assert.That(retrievedBox.Volume,Is.EqualTo(boxToAdd.Volume));
-
+        try
+        {
+            Box addedBox = repository.AddBox(boxToAdd);
+            Assert.NotNull(addedBox);
+            
+            Box retrievedBox = repository.GetBoxById(boxToAdd.Id);
+            Assert.NotNull(retrievedBox);
+            Assert.That(retrievedBox.Width, Is.EqualTo(boxToAdd.Width));
+            Assert.That(retrievedBox.Height, Is.EqualTo(boxToAdd.Height));
+            Assert.That(retrievedBox.Length, Is.EqualTo(boxToAdd.Length));
+            Assert.That(retrievedBox.Material,Is.EqualTo(boxToAdd.Material));
+            Assert.That(retrievedBox.Price,Is.EqualTo(boxToAdd.Price));
+            Assert.That(retrievedBox.InventoryCount,Is.EqualTo(boxToAdd.InventoryCount));
+            Assert.That(retrievedBox.Volume,Is.EqualTo(boxToAdd.Volume));
+            
+            Assert.Pass("Creating Box works!");
+            
+            repository.DeleteBoxById(retrievedBox.Id);
+        }
+        catch (Exception e)
+        {
+            Assert.Fail("Creating box doesn't work" + e);
+        }
+        
     }
 
 }
